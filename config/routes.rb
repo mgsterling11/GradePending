e023522060bb65bb5e2542a6b154fae462c0232d
mgsterling11 <m.g.sterling11@gmail.com>
2015-12-14 18:20:48 -0500

    includes about page, updates links

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :restaurants
   get '/' => 'welcome#index' 
   get '/search' => 'search#show'
-
+  get 'welcome/about' => 'welcome/about'
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in" => "sessions#new", :as => "log_in"
   get "sign_up" => "users#new", :as => "sign_up"
